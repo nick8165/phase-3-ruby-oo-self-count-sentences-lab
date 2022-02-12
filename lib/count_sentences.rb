595f3dc263c7da5d3424781badd2712cdd268cb3
nicholas shawn freeman <nick_freeman65@yahoo.com>
2022-02-12 15:32:45 -0600

    commit tree 1f394f14f638d78287d1fcf75d289d85dce8ca15

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,24 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.include? "."
   end
 
   def question?
-
+    self.include? "?"
   end
 
   def exclamation?
-
+    self.include? "!"
   end
 
   def count_sentences
-
+    text.gsub(/[\w']+/) do |word|
+      if word.is_of_certain_part_of_speech?
+        "___"  # Replace it with `"___"`.
+      else
+        word.length   # Put back the original word.
+      end
+     end
   end
 end
